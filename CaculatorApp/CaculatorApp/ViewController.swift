7db7d01447602fda23df221368ec2024cb630c21
Aby Mathew <abymathew0485@gmail.com>
2022-12-16 19:36:28 +0530

    Initial commit after project file added.

---
+++ CaculatorApp/CaculatorApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CaculatorApp
+//
+//  Created by Aby Mathew on 16/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CaculatorApp/CaculatorAppTests/CaculatorAppTests.swift
@@ -0,0 +1,14 @@
+//
+//  CaculatorAppTests.swift
+//  CaculatorAppTests
+//
+//  Created by Aby Mathew on 16/12/22.
+//
+
+import XCTest
+@testable import CaculatorApp
+
+class CaculatorAppTests: XCTestCase {
+
+
+}
