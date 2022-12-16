35f12e78f938bfa1a8dc9cef9fa4435456a13896
Aby Mathew <abymathew0485@gmail.com>
2022-12-16 19:39:55 +0530

    Addition added

--- CaculatorApp/CaculatorAppTests/CaculatorAppTests.swift
+++ CaculatorApp/CaculatorAppTests/CaculatorAppTests.swift
@@ -8,7 +8,26 @@
 import XCTest
 @testable import CaculatorApp
 
+final class Calculator {
+
+    var result: Double = 0.0
+
+    public func findSumOf(number1: Double, number2: Double) {
+        result = number1 + number2
+    }
+}
+
 class CaculatorAppTests: XCTestCase {
+
+
+    func test_addition_whenOnlyOneValue() {
+
+        let sut = Calculator()
+
+        sut.findSumOf(number1: 1, number2: 2)
+
+        XCTAssertEqual(sut.result, 3)
+    }
 
 
 }
